aee365a36d4bbd57a3840331b2e5e99aae79ae3b
ojuauml <joaolukkas092006@gmail.com>
2026-04-22 22:28:17 -0300

    Add estrutura switch case

--- src/main.c
+++ src/main.c
@@ -17,7 +17,27 @@ int main() {
     printf("Escolha uma Opcao: ");
     scanf("%d", &escolha);
     
-
+    switch (escolha)
+    {
+    case 1:
+     
+        break;
+    case 2:
+     
+        break;
+    case 3:
+     
+        break;
+    case 4:
+     
+        break;
+    case 5:
+     
+        break;
+    
+    default:
+        break;
+    }
     
     return 0;
 }
